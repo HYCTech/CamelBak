b9742076bfa99ef01f7bd94d7765eda5aa2c4cab
Kevin <anyboo@qq.com>
2018-11-07 12:08:29 +0800

    测试master protect是否有效

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 export default {
-    development: {
-        host: 'http://127.0.0.1:8080/api'
-    },
-    production: {
-        host: 'http://api.yx101.cn/hycDevelop'
-    }
+  development: {
+    host: 'http://127.0.0.1:8080/api'
+  },
+  production: {
+    host: 'http://api.yx101.cn/hycDevelop'
+  }
 }
